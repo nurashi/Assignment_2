57c54eae6ee119a969e945b98e2ac7fdb69fe813
Nurasyl Orazbek <nur1k.csgo1@gmail.com>
2024-12-31 11:30:40 +0500

    Add JUnit4 library and implement compareTo test for Student class

--- src/kz/nurashi/models/tests/PersonTest.java
+++ src/kz/nurashi/models/tests/PersonTest.java
@@ -1,4 +1,22 @@
 package kz.nurashi.models.tests;
 
+import kz.nurashi.models.ExtendsPersons.Student;
+import kz.nurashi.models.Person;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+
+// I use junit to test, so you must download this to run this test
 public class PersonTest {
+    @Test
+    public void testCompareTo(){
+        Person student1 = new Student("Harry", "Potter", 3.5);
+        Person student2 = new Student("Balenbay", "Balenbaeyv", 2.2);
+
+
+        int testResult = student1.compareTo(student2);
+        System.out.println(testResult);
+
+        assertTrue(testResult > 0);
+    }
 }
